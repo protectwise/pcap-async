b76a7dfaa23d9e89cd8eaac7b9d51582500120d9
ben kornmeier <ben@protectwise.com>
2019-12-06 15:16:38 -0700

    instead of doing the push front/back just going to use a iterator, in the future we can explore implimenting a priority poll system

--- benches/bench_capture.rs
+++ benches/bench_capture.rs
@@ -2,7 +2,7 @@
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 use futures::StreamExt;
 use log::*;
-use pcap_async::{Config, Handle, PacketStream};
+use pcap_async::{Config, Handle, PacketStream, BridgeStream};
 use std::path::PathBuf;
 
 fn bench_stream_from_large_file(b: &mut Bencher) {
@@ -53,6 +53,49 @@ fn bench_stream(c: &mut Criterion) {
     );
 }
 
+fn bench_stream_next_from_large_file_bridge(b: &mut Bencher) {
+    let _ = env_logger::try_init();
+
+    let pcap_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("resources")
+        .join("4SICS-GeekLounge-151020.pcap");
+
+    info!("Benchmarking against {:?}", pcap_path.clone());
+
+    b.iter(|| {
+        let rt = tokio::runtime::Runtime::new().expect("Failed to create runtime");
+
+        let clone_path = pcap_path.clone();
+
+        let handle1 = Handle::file_capture(clone_path.to_str().expect("No path found"))
+            .expect("No handle created");
+
+        let handle2 = Handle::file_capture(clone_path.to_str().expect("No path found"))
+            .expect("No handle created");
+
+        let mut cfg = Config::default();
+        cfg.with_max_packets_read(5000);
+
+        let packet_provider = BridgeStream::new(Config::default(), vec![handle1.clone(), handle2.clone()])
+            .expect("Failed to build");
+        let fut_packets = async move {
+            let mut packet_provider = packet_provider.boxed();
+            let mut packets = vec![];
+            while let Some(p) = packet_provider.next().await {
+                let p = p.expect("Could not get packets");
+                packets.extend(p);
+            }
+            packets
+        };
+        let packets = rt.block_on(fut_packets).len();
+
+        handle1.interrupt();
+        handle2.interrupt();
+
+        assert_eq!(packets, 246137 * 2);
+    });
+}
+
 fn bench_stream_next_from_large_file(b: &mut Bencher) {
     let _ = env_logger::try_init();
 
@@ -92,6 +135,18 @@ fn bench_stream_next_from_large_file(b: &mut Bencher) {
     });
 }
 
+fn bench_stream_next_bridge(c: &mut Criterion) {
+    let benchmark = criterion::Benchmark::new("4sics-bridge", bench_stream_next_from_large_file_bridge);
+
+    c.bench(
+        "stream_next",
+        benchmark
+            .sample_size(20)
+            .nresamples(1)
+            .measurement_time(std::time::Duration::from_secs(15)),
+    );
+}
+
 fn bench_stream_next(c: &mut Criterion) {
     let benchmark = criterion::Benchmark::new("4sics", bench_stream_next_from_large_file);
 
@@ -104,7 +159,7 @@ fn bench_stream_next(c: &mut Criterion) {
     );
 }
 
-criterion_group!(benches, bench_stream, bench_stream_next);
+criterion_group!(benches, bench_stream, bench_stream_next, bench_stream_next_bridge);
 
 // Benchmark: cargo bench --verbose
 
