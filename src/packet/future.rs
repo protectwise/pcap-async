b07d0cca90b6eb5a6c46da95e7e61ac3dc387bab
ben kornmeier <ben@protectwise.com>
2020-04-23 12:03:53 -0600

    disable timeout

--- src/packet/future.rs
+++ src/packet/future.rs
@@ -59,11 +59,13 @@ impl DispatchArgs {
             ev.poll_read_ready(cx, mio::Ready::readable())
                 .map_err(Error::Io)
         });
-        if let Some(dur) = timeout {
-            let _r = tokio::time::timeout(dur, f).await;
-        } else {
-            f.await?;
-        }
+        f.await?;
+        // if let Some(dur) = timeout {
+        //
+        //     let _r = tokio::time::timeout(dur, f).await;
+        // } else {
+        //     f.await?;
+        // }
         Ok(())
     }
 }
