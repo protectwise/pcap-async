b05c28377799a69b795835f94ef6f2655fb2d362
Ben <kornstar11@users.noreply.github.com>
2020-04-27 20:23:35 -0600

    Update bridge_stream.rs
    couple more logs

--- src/bridge_stream.rs
+++ src/bridge_stream.rs
@@ -128,11 +128,12 @@ impl<E: Fail + Sync + Send, T: Stream<Item = StreamItem<E>> + Sized + Unpin> Str
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
-        trace!("Interfaces: {:?}", this.stream_states.len());
         let states: &mut VecDeque<BridgeStreamState<E, T>> = this.stream_states;
         let max_buffer_time = this.max_buffer_time;
         let mut max_time_spread: Duration = Duration::from_millis(0);
         let mut delay_count = 0;
+        
+        trace!("Interfaces: {:?}", this.stream_states.len());
         for state in states.iter_mut() {
             max_time_spread = state.spread().max(max_time_spread);
             match Pin::new(&mut state.stream).poll_next(cx) {
@@ -142,6 +143,7 @@ impl<E: Fail + Sync + Send, T: Stream<Item = StreamItem<E>> + Sized + Unpin> Str
                     continue;
                 }
                 Poll::Ready(Some(Err(e))) => {
+                    trace!("got a error, passing upstream");
                     return Poll::Ready(Some(Err(e)));
                 }
                 Poll::Ready(None) => {
